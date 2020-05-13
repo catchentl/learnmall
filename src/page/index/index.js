67f18ed367db946246fd363bd3a934f91cc338e9
catchen <18374295061@163.com>
2020-05-13 23:09:13 +0800

    公共类方法新增

---
+++ src/page/index/index.js
@@ -0,0 +1,1 @@
+var _mm = require('util/mm.js');
--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,35 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
-module.exports={
-    entry: {
-      app : './src/index.js',
-      print : './src/print.js'
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-      contentBase: './dist'
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        title: 'Output Management'
-      })
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-  
+var config = {
+  mode : 'development', //模式 默认两种模式 production development', 
+  entry: {
+    // app : './src/index.js',
+    // print : './src/print.js'
+    'index'  :  ['./src/page/index/index.js'],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  resolve : {
+      alias : {
+        util : __dirname + '/src/util',
+        page : __dirname + '/src/page',
+        service : __dirname + '/src/service',
+        image : __dirname + '/src/image',
+      }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
+  output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
+  },
 }
+
+module.exports = config;
